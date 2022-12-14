c3245c83adad8412ca77262e229881b4d704023a
martinsvinicius <vinicius.victor.sm@gmail.com>
2022-12-14 00:41:27 -0300

    configurando prettier, eslint e chakra ui

---
+++ frontend/src/styles/colors.ts
@@ -0,0 +1,14 @@
+import { ColorHues } from '@chakra-ui/react'
+
+export const colors: Record<string, Partial<ColorHues>> = {
+  blue: {
+    50: '#E3ECFE',
+    700: '#273B73',
+  },
+  gray: {
+    50: '#FAFAFA',
+    100: '#EFEFEF',
+    200: '#C0C0C0',
+    500: '#373737',
+  },
+}
