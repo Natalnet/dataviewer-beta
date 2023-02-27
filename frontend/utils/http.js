3ff52ff21b2d134ee28da3c64fbe56028edc6f5a
Orivaldo Santana <orivaldo@gmail.com>
2023-02-27 19:22:31 -0300

    testando api no google cloud

--- frontend/utils/http.js
+++ frontend/utils/http.js
@@ -3,7 +3,9 @@ import { parseCookies } from "nookies"
 
 const { "nextautht1.token": token } = parseCookies()
 
-export const http = axios.create({ baseURL: "http://localhost:3333" })
+export const http = axios.create({
+   baseURL: "https://api-dataviewer-44gqlaax2a-uc.a.run.app",
+})
 
 if (token) {
    http.defaults.headers["Authoriation"] = `Bearer ${token}`
