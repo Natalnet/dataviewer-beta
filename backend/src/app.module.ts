c7ccd0f5971b31cecfcdcf899e43b080efd93f5e
Orivaldo Santana <orivaldo@gmail.com>
2023-02-27 19:37:26 -0300

    up

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_HOST, {
       authMechanism: 'DEFAULT',
-      dbName: 'dataviewer',
+      dbName: 'dataviewert1',
    
     }),
     UsersModule,
