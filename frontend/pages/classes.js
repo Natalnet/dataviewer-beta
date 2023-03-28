f38af7dba957f2fe8c2c50b698820781fa4673bb
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-03-28 10:01:41 -0300

    recuperando dados das turmas do backend

--- frontend/pages/classes.js
+++ frontend/pages/classes.js
@@ -59,23 +59,9 @@ export async function getServerSideProps(context) {
   }
 
   //console.log("Server side token: ", token)
-  const classes = [
-    {
-      id_class: '59dfe476-1325-433f-b676-c8df9a2c2072',
-      name: 'LoP 04 - 2020.1',
-      year: '2020',
-      semester: '1',
-      description: 'ECT2203 Lógica de Programação - Turma 04'
-    },
-    {
-      id_class: '6d04019e-905c-4d70-810a-a7f1fa34fe22',
-      name: 'LoP Turma 04 2020.2',
-      year: '2021',
-      semester: '1',
-      description: 'LoP Turma 04 2020.2'
-    }
-  ]
-
+  const { data } = await apiClient.get('classes')
+  console.log(data)
+  const classes = data
   return {
     props: {
       classes
