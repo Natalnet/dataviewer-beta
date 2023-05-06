f9f992d712102c6f71b0c3d6694cff69b0b05b18
Luis Martins <chateadorn@gmail.com>
2023-05-06 12:07:49 -0300

    importando modulo de performance corretamente

--- backend/src/modules/coordinator/coordinator.module.ts
+++ backend/src/modules/coordinator/coordinator.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PerformanceSchema } from './schemas/performance.schema';
+import { Performance } from './schemas/performance.schema';
 import { CoordinatorController } from './coordinator.controller';
 import { CoordinatorService } from './coordinator.service';
 
