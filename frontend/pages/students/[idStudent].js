dc8c745065a943ed89e3f61a7ecf444dcc772483
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-05-28 14:12:06 -0300

    Página inicial para um estudante

---
+++ frontend/pages/students/[idStudent].js
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+
+import styles from '../../styles/Home.module.css'
+
+function StudentPage() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <div className={styles.maincontainer}>
+        <h2>Estudante</h2>
+      </div>
+    </Box>
+  )
+}
+
+export default StudentPage
