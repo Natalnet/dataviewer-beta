63f50ea179c05836a554c3887b256561a29573e3
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-06-03 18:12:42 -0300

    registro da coleção 'studentlistgrades'

---
+++ backend/src/modules/students/dto/get-student-list-grades.dto.ts
@@ -0,0 +1,4 @@
+export class StudentListGradesDto {
+  fullName: string; 
+  progress: number;  
+}
--- backend/src/modules/students/students.module.ts
+++ backend/src/modules/students/students.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentListGrades, StudentListGradesSchema } from './schemas/studentlistgrades.schema';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 
 @Module({
+  imports: [ MongooseModule.forFeature([{name: StudentListGrades.name, schema: StudentListGradesSchema}])],
   controllers: [StudentsController],
   providers: [StudentsService]
 })
