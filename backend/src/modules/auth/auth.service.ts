8ca9ccc49969b8637faf67cb196e5fc4f0e240b7
Luis Martins <chateadorn@gmail.com>
2023-07-13 03:34:02 -0300

    verificando se o usuário possui e-mail confirmado antes de logar

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -18,12 +18,19 @@ export class AuthService {
 
     const isPasswordCorrect = await compare(password, user.password);
 
-    if (!isPasswordCorrect) return null;
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedException('A senha inserida está incorreta!')
+    }
+
+    if (!user.emailConfirmed) {
+      throw new BadRequestException('Você precisa confirmar seu e-mail para poder se autenticar!')
+    }
 
     return {
       id: user.id,
       email: user.email,
       name: user.name,
+      emailConfirmed: user.emailConfirmed
     };
   }
 
