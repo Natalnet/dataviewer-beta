4d713ad92f7aec0bb4ac4b6d6e6c912bdacdf6fb
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-09-12 18:10:04 -0300

    ajuste na cor de fundo

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -16,6 +16,14 @@ import {
 
 import { Close } from '@mui/icons-material/'
 
+import { makeStyles } from '@mui/styles'
+
+const loginStyles = makeStyles(theme => ({
+  customContainer: {
+    backgroundColor: '#e3ecfe'
+  }
+}))
+
 export default function Home() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -23,6 +31,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   const [alertMessage, setAlertMessage] = useState('')
   const { signIn } = useContext(AuthContext)
+  const classes = loginStyles()
 
   async function handleSubmit(e) {
     e.preventDefault()
