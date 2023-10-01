fb4afae4a77578101f7426cc87dc1c513ffc8cb1
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-10-01 18:07:18 -0300

    close #59

---
+++ frontend/pages/students/listgrades/index.js
@@ -0,0 +1,36 @@
+import { getAPIClient } from '../../../utils/axiosapi'
+import { parseCookies } from 'nookies'
+import StudentListPanel from '../../../components/students/StudentListPanel'
+
+function StudentListGradesPage({ data }) {
+  return <StudentListPanel data={data} />
+}
+
+export async function getServerSideProps(context) {
+  const apiClient = getAPIClient(context)
+
+  const { ['nextautht1.token']: token } = parseCookies(context)
+
+  const { ['nextautht1.mat']: mat } = parseCookies(context)
+
+  console.log('Mat: ' + mat)
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
+  let { data } = await apiClient.get(`students/listgradesbymat/${mat}`)
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default StudentListGradesPage
