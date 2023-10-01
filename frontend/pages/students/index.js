65265c3e2fce4dd0c6aa9a357df12ec58b23dd43
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-10-01 11:41:55 -0300

    retorna para página inicial do estudante onCancel

--- frontend/pages/students/index.js
+++ frontend/pages/students/index.js
@@ -1,3 +1,9 @@
+import MainCard from '../../components/layout/MainCard'
+
 export default function Students() {
-  return <>Students</>
+  return (
+    <>
+      <MainCard title="Resultado Geral">Students</MainCard>
+    </>
+  )
 }
