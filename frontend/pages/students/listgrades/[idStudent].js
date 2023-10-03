5be365f11de54d71d08b716141b6cf1b5a411ed4
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-10-03 18:31:23 -0300

    organizando página de listas do estudante

--- frontend/pages/students/listgrades/[idStudent].js
+++ frontend/pages/students/listgrades/[idStudent].js
@@ -1,5 +1,6 @@
 import StudentListPanel from '../../../components/students/StudentListPanel'
 import { getAPIClient } from '../../../utils/axiosapi'
+import { parseCookies } from 'nookies'
 
 function StudentListPage({ data }) {
   return <StudentListPanel data={data} />
@@ -8,6 +9,17 @@ function StudentListPage({ data }) {
 export async function getServerSideProps(context) {
   const { params } = context
   const apiClient = getAPIClient(context)
+
+  const { ['nextautht1.token']: token } = parseCookies(context)
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   let { data } = await apiClient.get(`students/listgrades/${params.idStudent}`)
 
