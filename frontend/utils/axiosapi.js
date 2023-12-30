7be07b575697810eb4847cda09d5a2cc78a49b7d
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-12-30 11:04:31 -0300

    ambiente dev: localhost

--- frontend/utils/axiosapi.js
+++ frontend/utils/axiosapi.js
@@ -1,19 +1,19 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios from "axios"
+import { parseCookies } from "nookies"
 
 export function getAPIClient(context) {
-  const { 'nextautht1.token': token } = parseCookies(context)
+   const { "nextautht1.token": token } = parseCookies(context)
 
-  const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL
-    //baseURL: 'https://api-dataviewer-44gqlaax2a-wn.a.run.app'
-    //baseURL: 'https://api-dataviewer-cao3vrugwa-wm.a.run.app'
-    //baseURL: 'http://localhost:3333'
-  })
+   const api = axios.create({
+      //baseURL: process.env.NEXT_PUBLIC_API_URL
+      //baseURL: 'https://api-dataviewer-44gqlaax2a-wn.a.run.app'
+      //baseURL: 'https://api-dataviewer-cao3vrugwa-wm.a.run.app'
+      baseURL: "http://localhost:3333",
+   })
 
-  if (token) {
-    api.defaults.headers['Authorization'] = `Bearer ${token}`
-  }
+   if (token) {
+      api.defaults.headers["Authorization"] = `Bearer ${token}`
+   }
 
-  return api
+   return api
 }
