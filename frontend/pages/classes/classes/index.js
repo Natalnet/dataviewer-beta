d00f67a419150606283769583b1f3e83d1884440
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-03-17 00:10:15 -0300

    update

--- frontend/pages/classes/classes/index.js
+++ frontend/pages/classes/classes/index.js
@@ -1,17 +1,17 @@
-import { Box, Grid, Paper } from "@mui/material";
+import { Box, Grid, Paper } from "@mui/material"
 
-import { H2 } from "../../../components/Typography";
-import { getAPIClient } from "../../../utils/axiosapi";
-import { parseCookies } from "nookies";
-import ClassClassTable from "../../../components/classes/ClassClassTable";
-import ClassFrequencyTable from "../../../components/classes/ClassFrequenciesTable";
+import { H2 } from "../../../components/Typography"
+import { getAPIClient } from "../../../utils/axiosapi"
+import { parseCookies } from "nookies"
+import ClassClassTable from "../../../components/classes/ClassClassTable"
+import ClassFrequencyTable from "../../../components/classes/ClassFrequenciesTable"
 
-import dynamic from "next/dynamic";
+import dynamic from "next/dynamic"
 
 const NoSSRClassFrequenciesChart = dynamic(
   () => import("../../../components/classes/ClassFrequenciesChart"),
   { ssr: false }
-);
+)
 
 function ClassClassesPage({ dataClasses, classFreqArray }) {
   //console.log(classFreqArray);
@@ -62,13 +62,13 @@ function ClassClassesPage({ dataClasses, classFreqArray }) {
         </Grid>
       </Box>
     </>
-  );
+  )
 }
 
 export async function getServerSideProps(context) {
-  const apiClient = getAPIClient(context);
-  const { ["nextautht1.token"]: token } = parseCookies(context);
-  const { ["nextautht1.lastClassCode"]: lastClassCode } = parseCookies(context);
+  const apiClient = getAPIClient(context)
+  const { ["nextautht1.token"]: token } = parseCookies(context)
+  const { ["nextautht1.lastClassCode"]: lastClassCode } = parseCookies(context)
 
   if (!token) {
     return {
@@ -76,31 +76,31 @@ export async function getServerSideProps(context) {
         destination: "/",
         permanent: false,
       },
-    };
+    }
   }
 
   //console.log(lastClassCode);
-  const { data } = await apiClient.get(`classes/classes/${lastClassCode}`);
+  const { data } = await apiClient.get(`classes/classes/${lastClassCode}`)
   //console.log(data);
-  const dataClasses = data;
+  const dataClasses = data
 
-  const dataFreqs = await apiClient.get(`classes/frequency/${lastClassCode}`);
+  const dataFreqs = await apiClient.get(`classes/frequency/${lastClassCode}`)
   //console.log(dataFreqs.data.classFreqs);
-  const studentNumber = parseFloat(dataFreqs.data.studentNumber);
-  let classFreqArray = [];
+  const studentNumber = parseFloat(dataFreqs.data.studentNumber)
+  let classFreqArray = []
   if (dataFreqs.status === 200) {
     for (let i = 0; i < dataClasses.length; i++) {
-      let row = {};
+      let row = {}
       row["frequencies"] =
         dataFreqs.data.classFreqs[dataClasses[i].date] == undefined
           ? 0
-          : dataFreqs.data.classFreqs[dataClasses[i].date];
-      row["date"] = dataClasses[i].date;
-      row["classTitle"] = dataClasses[i].classTitle;
+          : dataFreqs.data.classFreqs[dataClasses[i].date]
+      row["date"] = dataClasses[i].date
+      row["classTitle"] = dataClasses[i].classTitle
       row["percentage"] = Math.floor(
         (parseFloat(row["frequencies"]) / studentNumber) * 100
-      );
-      classFreqArray.push(row);
+      )
+      classFreqArray.push(row)
       //console.log(row);
     }
   }
@@ -110,7 +110,7 @@ export async function getServerSideProps(context) {
       classFreqArray,
       dataClasses,
     },
-  };
+  }
 }
 
-export default ClassClassesPage;
+export default ClassClassesPage
