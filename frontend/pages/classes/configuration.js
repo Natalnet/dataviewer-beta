783f616beb8c164539ba5713c3a8026ceb0f8f26
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-03-17 23:20:28 -0300

    Upload file initial web interface

--- frontend/pages/classes/configuration.js
+++ frontend/pages/classes/configuration.js
@@ -1,12 +1,42 @@
 import { getAPIClient } from "../../utils/axiosapi"
 import { parseCookies } from "nookies"
 
+import { useState } from "react"
+import { useDropzone } from "react-dropzone"
+import { readAsText } from "promise-file-reader"
+
 import MainCard from "../../components/layout/MainCard"
 
 export default function Configuration() {
+  const [fileLines, setFileLines] = useState([])
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: ".csv, .txt",
+    onDrop: async (acceptedFiles) => {
+      try {
+        const file = acceptedFiles[0]
+        const content = await readAsText(file)
+        const lines = content.split("\n")
+        setFileLines(lines)
+      } catch (err) {
+        console.error(err)
+      }
+    },
+  })
+
   return (
     <>
-      <MainCard title="Configurações"> </MainCard>
+      <MainCard title="Configurações">
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>
+            Arraste e solte um arquivo txt aqui, ou clique para selecionar um
+            arquivo
+          </p>
+          {fileLines.map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+      </MainCard>
     </>
   )
 }
