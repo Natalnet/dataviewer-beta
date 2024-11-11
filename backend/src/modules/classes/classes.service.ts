9e5d736826476cc9c3cbaaa272679376b874be51
celinehelena <celine.andrade.710@ufrn.edu.br>
2024-11-10 21:00:11 -0300

    feat: rota para cadastrar as submissões diárias de uma turma

--- backend/src/modules/classes/classes.service.ts
+++ backend/src/modules/classes/classes.service.ts
@@ -1,6 +1,7 @@
 import {
   Injectable,
   BadRequestException,
+  ForbiddenException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -277,7 +278,7 @@ export class ClassesService {
       .findOne({ classCode: class_Code })
       .exec();
 
-    if (!data) return null;
+    if (!data) return [];
     return {
       datas: data.counts,
     };
