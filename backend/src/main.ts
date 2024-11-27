dd62332f4336829e6733d77abb5b1c542aa63ca4
celinehelena <celine.andrade.710@ufrn.edu.br>
2024-11-26 21:49:50 -0300

    feat: instalando as dependencias para o swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Docs')
+    .setDescription('Dataviewer API description')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger/docs', app, documentFactory);
+
   await app.listen(3333);
 }
 bootstrap();
