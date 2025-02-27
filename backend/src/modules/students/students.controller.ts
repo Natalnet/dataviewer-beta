f06ba57a02188904e389568e1e225a4f23b20eeb
celinehelena <celine.andrade.710@ufrn.edu.br>
2025-02-27 17:49:37 -0300

    Configurando a documentação de students e users

--- backend/src/modules/students/students.controller.ts
+++ backend/src/modules/students/students.controller.ts
@@ -1,24 +1,83 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { StudentsService } from './students.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('students')
 export class StudentsController {
   constructor(private studentService: StudentsService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get('listgrades/:id')
+  @ApiBearerAuth('KEY_AUTH')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: 'Consult a student performance in each exercise list',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          fullName: { type: 'string' },
+          progress: { type: 'number' },
+        },
+      },
+    },
+  })
   findListGradesById(@Param('id') id: string) {
     return this.studentService.findStudentListGrades(id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('listgradesbymat/:mat')
+  @ApiBearerAuth('KEY_AUTH')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: 'Get student list grades by registration',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Student list grades retrieved successfully.',
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          fullName: {
+            type: 'string',
+          },
+          progress: {
+            type: 'number',
+          },
+        },
+      },
+    },
+  })
   findListGradesByMat(@Param('mat') mat: string) {
     return this.studentService.findStudentListGradesByMat(mat);
   }
 
   @Get('listunitgrades/:mat')
+  @ApiBearerAuth('KEY_AUTH')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: 'Retrieve student unit grades',
+    description:
+      'Returns the mean grades for units U1, U2, and U3 for the specified student registration number.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved unit grades.',
+    schema: {
+      type: 'object',
+      properties: {
+        meanU1: { type: 'string' },
+        meanU2: { type: 'string' },
+        meanU3: { type: 'string' },
+      },
+    },
+  })
   findListUnitGrades(@Param('mat') mat: string) {
     return this.studentService.findStudentListUnitGrades(mat);
   }
--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -13,24 +13,97 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RequestWithUser } from 'src/types/requests';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully.',
+    schema: {
+      type: 'object',
+      properties: {
+        acessToken: { type: 'string' },
+        id: { type: 'string' },
+        email: { type: 'string' },
+        name: { type: 'string' },
+        emailConfirmed: { type: 'string' },
+        profile: { type: 'string' },
+        avatar: { type: 'string' },
+        registrationNumber: { type: 'string' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request - The input data is invalid.',
+  })
+  @ApiResponse({
+    status: 403,
+    description:
+      'Forbidden - User does not have permission to perform this operation.',
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('info')
+  @ApiOperation({
+    summary: 'Get profile information of the authenticated user',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User profile retrieved successfully.',
+    schema: {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        email: { type: 'string' },
+        name: { type: 'string' },
+        profile: { type: 'string' },
+        avatar: { type: 'string' },
+        registrationNumber: { type: 'string' },
+      },
+    },
+  }) // Resposta esperada
+  @ApiResponse({
+    status: 401,
+    description:
+      'Unauthorized - Authentication credentials are missing or invalid.',
+  })
+  @ApiBearerAuth('KEY_AUTH')
+  @UseGuards(JwtAuthGuard)
   getProfile(@Request() req: RequestWithUser) {
     return this.usersService.findOne(req.user.userId);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch('update-account')
+  @ApiBearerAuth('KEY_AUTH')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Update authenticated user account information' })
+  @ApiResponse({
+    status: 200,
+    description: 'User account updated successfully.',
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request - The input data is invalid.',
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      'Unauthorized - Authentication credentials are missing or invalid.',
+  })
   update(
     @Request() req: RequestWithUser,
     @Body() updateUserDto: UpdateUserDto,
@@ -38,8 +111,19 @@ export class UsersController {
     return this.usersService.update(req.user.userId, updateUserDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('delete-account')
+  @ApiBearerAuth('KEY_AUTH')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Delete authenticated user account' })
+  @ApiResponse({
+    status: 200,
+    description: 'User account deleted successfully.',
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      'Unauthorized - Authentication credentials are missing or invalid.',
+  })
   remove(@Request() req: RequestWithUser) {
     return this.usersService.remove(req.user.userId);
   }
