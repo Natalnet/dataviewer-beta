dcf05d63ede1a115cc0926c8035787a784977dce
Johnata Matheus <johnata@paytour.com.br>
2025-04-27 00:53:17 -0300

    feat: create methods of signin, signup, refresh token  in auth-controller

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { RequestWithLocalUser } from 'src/types/requests';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { SignUpDto } from './dto/sign-up.dto';
+import { GetUser } from '../../shared/decorators/get-user.decorator';
+import { UserResponseDto } from '../users/dto/user-response.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(@Request() req: RequestWithLocalUser) {
-    return this.authService.login(req.user);
+  @Post('signIn')
+  async signIn(@GetUser() user: UserResponseDto) {
+    return this.authService.signIn(user);
+  }
+
+  @Post('signUp')
+  async signUp(@Body() user: SignUpDto) {
+    return this.authService.signUp(user);
+  }
+
+  @Post('refresh')
+  async refresh(@Body('refresh_token') refreshToken: string) {
+    return this.authService.refreshToken(refreshToken);
   }
 }
