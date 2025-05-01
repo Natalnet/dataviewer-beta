844a6dd378e0406014c7f671caf7954095a77313
Johnata Matheus <johnata@paytour.com.br>
2025-05-01 14:56:23 -0300

    feat: add response cookies in methods signin and refresh

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,18 +1,33 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { SignUpDto } from './dto/sign-up.dto';
 import { GetUser } from '../../shared/decorators/get-user.decorator';
 import { UserResponseDto } from '../users/dto/user-response.dto';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  private readonly cookieExpiration: number;
+
+  constructor(private authService: AuthService, private configService: ConfigService) {
+    this.cookieExpiration = this.configService.get<number>('COOKIE_REFRESH_TOKEN_EXPIRATION');
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('signIn')
-  async signIn(@GetUser() user: UserResponseDto) {
-    return this.authService.signIn(user);
+  async signIn(@GetUser() user: UserResponseDto, @Res({ passthrough: true }) res: Response) {
+    const { accessToken, refreshToken } = await this.authService.signIn(user);
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      maxAge: this.cookieExpiration,
+      sameSite: 'strict',
+      path: '/auth',
+    });
+
+    return { accessToken };
   }
 
   @Post('signUp')
@@ -21,7 +36,22 @@ export class AuthController {
   }
 
   @Post('refresh')
-  async refresh(@Body('refresh_token') refreshToken: string) {
-    return this.authService.refreshToken(refreshToken);
+  async refresh(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    const refreshToken = req.cookies['refresh_token'];
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token not found.');
+    }
+
+    const tokens = await this.authService.refreshToken(refreshToken);
+
+    res.cookie('refresh_token', tokens.refreshToken, {
+      httpOnly: true,
+      maxAge: this.cookieExpiration,
+      sameSite: 'strict',
+      path: '/auth',
+    });
+
+    return { accessToken: tokens.accessToken };
   }
 }
