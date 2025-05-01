2fb0dcd52e314f10a4814733bfcc85b18463fc9f
Johnata Matheus <johnata@paytour.com.br>
2025-05-01 14:54:21 -0300

    feat: add cookie parser middleware in bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
